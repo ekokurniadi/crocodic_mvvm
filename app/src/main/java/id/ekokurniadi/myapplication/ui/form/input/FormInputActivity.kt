71797cc5308862737cab1a7f814b3f79ddb80a7e
ekokurniadi <ekokurniadi.02@gmail.com>
2021-10-18 16:40:58 +0700

    menambahkan form input activity

---
+++ app/src/main/java/id/ekokurniadi/myapplication/ui/form/input/FormInputActivity.kt
@@ -0,0 +1,12 @@
+package id.ekokurniadi.myapplication.ui.form.input
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormInputActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_input)
+    }
+}
---
+++ app/src/main/java/id/ekokurniadi/myapplication/ui/form/input/FormInputViewModel.kt
@@ -0,0 +1,8 @@
+package id.ekokurniadi.myapplication.ui.splash
+
+import id.ekokurniadi.myapplication.ui.base.BaseViewModel
+import javax.inject.Inject
+
+class SplashViewModel @Inject constructor(): BaseViewModel(){
+
+}
